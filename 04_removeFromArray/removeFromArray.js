0f83cf851d5b863dbd5f61cd894da3f555b43558
CNVRSN <69506438+CNVRSN@users.noreply.github.com.>
2024-01-08 19:35:22 +0100

    Solved all exercises 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(myArray, ...args) {
+    return myArray.filter(x => !args.includes(x));
+};  
 
 // Do not edit below this line
 module.exports = removeFromArray;
